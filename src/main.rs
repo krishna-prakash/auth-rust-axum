80afbc182b478ffb121b85451214f44372bdd845
prakash296 <krishnakprakash296@gmail.com>
2026-04-29 21:45:37 +0530

    users table migrations added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use std::env;
+
 use axum::{Router, routing::get};
+use dotenvy::dotenv;
+use sqlx::{pool::PoolConnection, postgres::PgPoolOptions};
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let db_url = env::var("DB_URL").expect("not able to load db  url");
+    println!("This is {} database url", db_url);
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+        .expect("db connection failed");
+
+    sqlx::migrate!().run(&pool).await.expect("migration failed");
     let app = Router::new()
         .route("/", get(|| async { "hello world".to_string() }));
 
